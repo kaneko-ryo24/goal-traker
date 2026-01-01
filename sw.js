fdd826249629024eb4283c9bb5758efbc90c4f44
kaneko-ryo24 <otimusya24@gmail.com>
2026-01-01 10:45:20 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'goal-tracker-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('キャッシュを開きました');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエスト時にキャッシュから返す
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがあればそれを返す、なければネットワークから取得
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('古いキャッシュを削除:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
